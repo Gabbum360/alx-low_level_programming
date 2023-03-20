f9ecc4be15555df67e799297663b21a1fde98feb
gabbum360 <gabbum360@gmail.com>
2023-03-20 07:02:03 -0700

    new edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
-#!/bin/bash 
 #include <stdio.h>
-  /**
-   *   * main - this an important  function
-   *     *
-   *       * Return: always 0
-   * /
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return (0)
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
